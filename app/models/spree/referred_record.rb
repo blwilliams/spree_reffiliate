2dd9fbadce3a4c12bee3c82cabb34a21122b18d0
Blair Williams <blair.l.williams@intel.com>
2018-03-18 12:40:32 -0700

    Updates for User to Affiliate association for spree 3.4

--- app/models/spree/referred_record.rb
+++ app/models/spree/referred_record.rb
@@ -1,9 +1,9 @@
 module Spree
   class ReferredRecord < Spree::Base
     belongs_to :referral
-    belongs_to :user, class_name: Spree.user_class.to_s
     belongs_to :affiliate
     belongs_to :store_credit
+    belongs_to :user, class_name: Spree.user_class.to_s
 
     validates_presence_of :user
   end
