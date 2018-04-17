4d07a7ceb3ab8184075e19baf66488b8279358fa
Blair Williams <blair.l.williams@intel.com>
2018-04-17 08:53:10 -0700

    Updated user model.

--- app/models/spree/affiliate.rb
+++ app/models/spree/affiliate.rb
@@ -57,7 +57,7 @@ module Spree
     private
 
       def create_user
-        @user = User.find_or_initialize_by(username: username)
+        @user = Spree::User.find_or_initialize_by(email: email)
         self.active_on_create = true if user.persisted?
         affiliate_role = Spree::Role.affiliate
         user.spree_roles << affiliate_role unless user.spree_roles.include?(affiliate_role)
